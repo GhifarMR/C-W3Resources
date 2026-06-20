c99287365cb792fef3fab092555f3773ed8df6be
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2026-06-20 20:58:14 +0700

    add 001-Print.c

---
+++ 001-Print.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Name\t: Alexandra Abramov\n");
+    printf("DOB\t: July 14, 1975\n");
+    printf("Mobile\t: 99-999999999\n");
+    
+    return 0;
+}
